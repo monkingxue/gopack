a58513f899a36d1ae19abdd6abd6f4184d8e194f
qiuyuhang <imqyh@outlook.com>
2017-10-22 00:34:58 +0800

    add frame func

--- module.go
+++ module.go
@@ -9,7 +9,26 @@ type (
 		path    string
 		code    string
 		ast     *ast.Program
-		imports *map[string]*Module
-		exports *map[string]*Module
+		imports map[string]*Module
+		exports map[string]*Module
+		visited bool
 	}
 )
+
+func (m *Module) Dfs() bool {
+	if (m.visited) {
+		return true
+	}
+	m.visited = true
+	for _, v := range m.imports {
+		if (v.Dfs()) {
+			return true
+		}
+	}
+	m.visited = false
+	return false
+}
+
+func CreateModule(path string, source string) *Module {
+	return &Module{}
+}
