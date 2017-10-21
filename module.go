9492de1eb640376fe44ceaa185d727ab6f407ec8
monkingxue <1360662499@qq.com>
2017-10-21 21:50:53 +0800

    [refactor] clear module

--- module.go
+++ module.go
@@ -2,37 +2,14 @@ package main
 
 import (
 	"github.com/robertkrimen/otto/ast"
-	"github.com/robertkrimen/otto/parser"
-	"log"
 )
 
 type (
-	ModuleTable map[int]string
-
 	Module struct {
-		bundle   *Bundle
-		path     string
-		code     string
-		comments []string
-		ast      *ast.Program
-		imports  *ModuleTable
-		exports  *ModuleTable
+		path    string
+		code    string
+		ast     *ast.Program
+		imports *map[int]string
+		exports *map[int]string
 	}
 )
-
-func (self *Module) Constructor(bundle *Bundle, path string, code string) {
-	self.bundle, self.path, self.code = bundle, path, code
-	self.imports = new(ModuleTable)
-	self.exports = new(ModuleTable)
-	ast, err := parser.ParseFile(nil, "", code, 0)
-	if err != nil {
-		log.Fatal("Error parse!")
-	}
-	self.ast = ast
-
-	self.analyse()
-}
-
-func (self *Module) analyse() {
-
-}
